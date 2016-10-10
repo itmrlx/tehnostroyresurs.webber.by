28be9662ca940f18fc91f9b9c55c6f37213f72fb
itmrlx <itmrlx@gmail.com>
2016-10-10 09:43:44 +0300

    services inside
    pp desktop services inside page

---
+++ services-inside.php
@@ -0,0 +1,38 @@
+<?php include 'header.php'; ?>
+
+	<div class="wrapper services-header">
+		<div class="container">
+			<h1 class="title">Земляные работы</h1>
+			<p>В основу своей работы «Техстройресурс Плюс» закладывает комплексный подход, в результате которого клиент получает «под ключ» любой объект — будь то офис, административное здание, жилье, производственное помещение и т.д. Наша организация в течение многих лет выступала как генподрядчик при строитель­стве многих объектов.</p>
+		</div>
+	</div>
+
+	<div class="container services-content content">
+		<h2>Заголовок 1</h2>
+		<p>В основу своей работы «Техстройресурс Плюс» закладывает комплексный подход, в результате которого клиент получает «под ключ» любой объект — будь то офис, административное здание, жилье, производственное помещение и т.д. Наша организация в течение многих лет выступала как генподрядчик при строитель­стве многих объектов.</p>
+		<br>
+		<h2>Маркированный список</h2>
+		<ul>
+			<li>Пункт номер один</li>
+			<li>Пункт номер два</li>
+			<li>Пункт номер три</li>
+			<li>Пункт номер четыре</li>
+			<li>Пункт номер пять</li>
+		</ul>
+	</div>
+
+	<div class="wrapper services">
+		<div class="title-project">РЕАЛИЗОВАННЫЕ ПРОЕКТЫ</div>
+		<a href="#" class="col-xs-4 service-item" style="background-image: url(img/portfolio1.jpg);">
+			<div class="text"><div class="title">ТОРГОВЫЙ ЦЕНТР «ЛЕНИНГРАД»</div></div>
+		</a>
+		<a href="#" class="col-xs-4 service-item" style="background-image: url(img/portfolio2.jpg);">
+			<div class="text"><div class="title">МНОГОКВАРТИРНЫЙ ЖИЛОЙ ДОМ ПО УЛИЦЕ<br>КАРАСТАЯНОВА</div></div>
+		</a>
+		<a href="#" class="col-xs-4 service-item" style="background-image: url(img/portfolio3.jpg);">
+			<div class="text"><div class="title">АДМИНИСТРАТИВНОЕ ЗДАНИЕ ЦЕНТРАЛЬНОГО<br>РАЙОНА</div></div>
+		</a>
+		<div class="clearfix"></div>
+	</div>
+
+<?php include 'footer.php'; ?>
