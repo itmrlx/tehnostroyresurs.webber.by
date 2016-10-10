dc8b3670ac513084fdc6c2fb46e3b61c39836ab7
itmrlx <itmrlx@gmail.com>
2016-10-10 10:21:15 +0300

    news page
    pp desktop news page

---
+++ news.php
@@ -0,0 +1,94 @@
+<?php include 'header.php'; ?>
+
+	<div class="wrapper purple-block news-purple">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-3">
+					<h1 class="title">Новости</h1>
+				</div>
+				<div class="col-xs-9 purple-menu-container">
+					<ul class="purple-menu">
+						<li class="current-menu-item"><a href="#">ВСЕ НОВОСТИ</a></li>
+						<li><a href="#">СТРОИТЕЛЬСТВО</a></li>
+						<li><a href="#">ТЕХНОЛОГИИ</a></li>
+						<li><a href="#">ДИЗАЙН</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- news -->
+		<div class="container news-page">
+			<!-- news -->
+			<div class="row news">
+				<div class="col-xs-4">
+					<div class="news-item" style="background-image: url(img/news-1.png);">
+						<div class="date">22.03.16</div>
+						<h2 class="title">Торгово- развлекательный центр "ЛенiнГрад"</h2>
+						<div class="desc">В центре Минска на пересечении улиц Ленина и Смоленской приобрел свои реальные очертания многофункциональный торговый комплекс, получивший громкое название «ЛенiнГрад».</div>
+						<a href="#" class="btn btn-transparent">ПОДРОБНЕЕ</a>
+					</div>
+				</div>
+				<div class="col-xs-4">
+					<div class="news-item" style="background-image: url(img/news-2.png);">
+						<div class="date">07.09.16</div>
+						<h2 class="title">Окончание строительства офисного здания по улице Веры Хоружей</h2>
+						<div class="desc">Реальные очертания многофункциональный торговый комплекс, получивший громкое название «ЛенiнГрад».</div>
+						<a href="#" class="btn btn-transparent">ПОДРОБНЕЕ</a>
+					</div>
+				</div>
+				<div class="col-xs-4">
+					<div class="news-item" style="background-image: url(img/news-3.png);">
+						<div class="date">27.12.16</div>
+						<h2 class="title">Каждому — по коттеджу</h2>
+						<div class="desc">Реальные очертания многофункциональный торговый комплекс, получивший громкое название «ЛенiнГрад». В центре Минска на пересечении улиц Ленина и Смоленской приобрел свои реальные очертания торговый комплекс, получивший громкое название.</div>
+						<a href="#" class="btn btn-transparent">ПОДРОБНЕЕ</a>
+					</div>
+				</div>
+			</div>
+			<div class="row news">
+				<div class="col-xs-4">
+					<div class="news-item" style="background-image: url(img/news-1.png);">
+						<div class="date">22.03.16</div>
+						<h2 class="title">Торгово- развлекательный центр "ЛенiнГрад"</h2>
+						<div class="desc">В центре Минска на пересечении улиц Ленина и Смоленской приобрел свои реальные очертания многофункциональный торговый комплекс, получивший громкое название «ЛенiнГрад».</div>
+						<a href="#" class="btn btn-transparent">ПОДРОБНЕЕ</a>
+					</div>
+				</div>
+				<div class="col-xs-4">
+					<div class="news-item" style="background-image: url(img/news-2.png);">
+						<div class="date">07.09.16</div>
+						<h2 class="title">Окончание строительства офисного здания по улице Веры Хоружей</h2>
+						<div class="desc">Реальные очертания многофункциональный торговый комплекс, получивший громкое название «ЛенiнГрад».</div>
+						<a href="#" class="btn btn-transparent">ПОДРОБНЕЕ</a>
+					</div>
+				</div>
+				<div class="col-xs-4">
+					<div class="news-item" style="background-image: url(img/news-3.png);">
+						<div class="date">27.12.16</div>
+						<h2 class="title">Каждому — по коттеджу</h2>
+						<div class="desc">Реальные очертания многофункциональный торговый комплекс, получивший громкое название «ЛенiнГрад». В центре Минска на пересечении улиц Ленина и Смоленской приобрел свои реальные очертания торговый комплекс, получивший громкое название.</div>
+						<a href="#" class="btn btn-transparent">ПОДРОБНЕЕ</a>
+					</div>
+				</div>
+			</div>
+			<!-- / news -->
+			<div class="clearfix"></div>
+			<nav class="centered">
+				<ul class="pagination">
+					<li><a href="#">1</a></li>
+					<li class="active"><a href="#">2</a></li>
+					<li><a href="#">3</a></li>
+					<li><a href="#">4</a></li>
+					<li><a href="#">5</a></li>
+					<li>
+						<a href="#" aria-label="Next">
+							<span aria-hidden="true">&raquo;</span>
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
+
+<?php include 'footer.php'; ?>
