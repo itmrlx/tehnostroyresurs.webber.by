4cdba62713aa0eca78c87b01536133a0f1d3e297
itmrlx <itmrlx@gmail.com>
2016-10-10 10:57:31 +0300

    about page
    pp desktop about page
    other fixes

--- about.php
+++ about.php
@@ -5,21 +5,30 @@
 			<div class="row">
 				<div class="col-xs-12 purple-menu-container">
 					<ul class="purple-menu">
-						<li><a href="#">О КОМПАНИИ</a></li>
+						<li class="current-menu-item"><a href="#">О КОМПАНИИ</a></li>
 						<li><a href="#">ИСТОРИЯ</a></li>
-						<li class="current-menu-item"><a href="#">СОТРУДНИКИ</a></li>
+						<li><a href="#">СОТРУДНИКИ</a></li>
 						<li><a href="#">ОТЗЫВЫ</a></li>
 					</ul>
 				</div>
 				<div class="col-xs-12 description">
-					<h1 class="title">Сотрудники</h1>
+					<h1 class="title">О компании</h1>
 					<p>В основу своей работы «Техстройресурс Плюс» закладывает комплексный подход, в результате которого клиент получает «под ключ» любой объект — будь то офис, административное здание, жилье, производственное помещение и т.д. Наша организация в течение многих лет выступала как генподрядчик при строитель­стве многих объектов. ООО «Техстройресурс Плюс» имеет в своем составе специалистов и производственный потенциал для выполнения различных видов строительно-монтажных работ и собственными силами выполняет до 70% работ по строительству объектов.</p>
+				</div>
+				<div class="col-xs-12 attestat">
+					<div class="title">АТТЕСТАТЫ СООТВЕТСТВИЯ</div>
+					<a href="img/attestat.jpg" class="fancybox" data-fancybox-group="attestat-group"><img src="img/attestat.jpg" alt="attestat"></a>
+					<a href="img/attestat.jpg" class="fancybox" data-fancybox-group="attestat-group"><img src="img/attestat.jpg" alt="attestat"></a>
+					<a href="img/attestat.jpg" class="fancybox" data-fancybox-group="attestat-group"><img src="img/attestat.jpg" alt="attestat"></a>
+					<a href="img/attestat.jpg" class="fancybox" data-fancybox-group="attestat-group"><img src="img/attestat.jpg" alt="attestat"></a>
+					<a href="img/attestat.jpg" class="fancybox" data-fancybox-group="attestat-group"><img src="img/attestat.jpg" alt="attestat"></a>
+					<a href="img/attestat.jpg" class="fancybox" data-fancybox-group="attestat-group"><img src="img/attestat.jpg" alt="attestat"></a>
 				</div>
 			</div>
 		</div>
 	</div>
 
-	<!-- personal page -->
+	<!-- personal -->
 	<div class="container personal-page">
 		<div class="row">
 			<!-- personal items -->
@@ -63,44 +72,72 @@
 				</div>
 				<div class="desc">Мы предлагаем строительство и отделку объектов «под ключ»: офисы, административные здания, жилье, производственные помещения. В основе работы компании лежат приниципы профессионализма и доступной стоимости услуг.</div>
 			</div>
-			<div class="col-xs-6 personal-item">
-				<div class="title-container">
-					<img src="img/personal.jpg" alt="personal">
-					<div class="text">
-						<div class="name">Щелкунчик Алла Валерьвна</div>
-						<div class="st">МЕНЕДЖЕР ПРОЕКТОВ</div>
-					</div>
-				</div>
-				<div class="desc">Мы предлагаем строительство и отделку объектов «под ключ»: офисы, административные здания, жилье, производственные помещения. В основе работы компании лежат приниципы профессионализма и доступной стоимости услуг.</div>
-			</div>
-			<div class="col-xs-6 personal-item">
-				<div class="title-container">
-					<img src="img/personal.jpg" alt="personal">
-					<div class="text">
-						<div class="name">Щелкунчик Алла Валерьвна</div>
-						<div class="st">МЕНЕДЖЕР ПРОЕКТОВ</div>
-					</div>
-				</div>
-				<div class="desc">Мы предлагаем строительство и отделку объектов «под ключ»: офисы, административные здания, жилье, производственные помещения. В основе работы компании лежат приниципы профессионализма и доступной стоимости услуг.</div>
-			</div>
 			<!-- / personal items -->
 			<div class="clearfix"></div>
-			<nav class="centered">
-				<ul class="pagination">
-					<li><a href="#">1</a></li>
-					<li class="active"><a href="#">2</a></li>
-					<li><a href="#">3</a></li>
-					<li><a href="#">4</a></li>
-					<li><a href="#">5</a></li>
-					<li>
-						<a href="#" aria-label="Next">
-							<span aria-hidden="true">&raquo;</span>
-						</a>
-					</li>
-				</ul>
-			</nav>
+			<a href="#" class="btn btn-primary alignright">ПОКАЗАТЬ ВСЕХ СОТРУДНИКОВ</a>
+			<div class="clearfix"></div>
 		</div>
 	</div>
 
+	<!-- reviews page -->
+	<div class="about-review">
+		<div class="container reviews-page">
+			<div class="title-r">ОТЗЫВЫ КЛИЕНТОВ</div>
+			<!-- review items -->
+			<div class="review-block">
+				<div class="row">
+					<div class="col-xs-10 col-xs-offset-1 white">
+						<div class="row">
+							<div class="col-xs-10 center">
+								<h2 class="title">ООО «Бонди»</h2>
+								<div class="desc">Выполнение строительно-монтажных работ</div>
+								<div class="text">«Для достижения высокого качества работ и их своевременного осуществления Подрядчик при производстве работ использует различные виды опалубки, обеспечивает строительный процесс современной техникой, квалифицированными кадрами»</div>
+							</div>
+							<div class="col-xs-2 right">
+								<a href="img/review-big.jpg" class="fancybox"><img src="img/review.png" alt="Отзыв"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="review-block">
+				<div class="row">
+					<div class="col-xs-10 col-xs-offset-1 white">
+						<div class="row">
+							<div class="col-xs-10 center">
+								<h2 class="title">ООО «Бонди»</h2>
+								<div class="desc">Выполнение строительно-монтажных работ</div>
+								<div class="text">«Для достижения высокого качества работ и их своевременного осуществления Подрядчик при производстве работ использует различные виды опалубки, обеспечивает строительный процесс современной техникой, квалифицированными кадрами»</div>
+							</div>
+							<div class="col-xs-2 right">
+								<a href="img/review-big.jpg" class="fancybox"><img src="img/review.png" alt="Отзыв"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="review-block">
+				<div class="row">
+					<div class="col-xs-10 col-xs-offset-1 white">
+						<div class="row">
+							<div class="col-xs-10 center">
+								<h2 class="title">ООО «Бонди»</h2>
+								<div class="desc">Выполнение строительно-монтажных работ</div>
+								<div class="text">«Для достижения высокого качества работ и их своевременного осуществления Подрядчик при производстве работ использует различные виды опалубки, обеспечивает строительный процесс современной техникой, квалифицированными кадрами»</div>
+							</div>
+							<div class="col-xs-2 right">
+								<a href="img/review-big.jpg" class="fancybox"><img src="img/review.png" alt="Отзыв"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- / review items -->
+			<div class="clearfix"></div>
+			<div class="centered">
+				<a href="#" class="btn btn-primary">ЧИТАТЬ ВСЕ</a>
+			</div>
+		</div>
+	</div>
 
 <?php include 'footer.php'; ?>
